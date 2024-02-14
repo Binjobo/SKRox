7704f27cd93f3f69e01699e173c0b3a9af0c77b6
Binjobo <101441601+Binjobo@users.noreply.github.com>
2024-02-14 22:09:37 +0800

    NavBar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,33 @@
+import { useCookies } from "react-cookie";
 import { NavLink } from "react-router-dom";
 
-export default function NavBar({ user }) {
+const NavBar = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const authToken = cookies.AuthToken;
+
   return (
-    <nav>
-      SKRox
-      {user && (
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
-          </li>
-          {/* <li>
-          <NavLink to="/preference">Preference</NavLink>
-        </li>  */}
-          <li>
-            <NavLink to="/match">Match</NavLink>
-          </li>
-          <li>
-            <NavLink to="/chat">Chat</NavLink>
-          </li>
-        </ul>
-      )}
-    </nav>
+    <>
+      <nav>
+        SKRox-Logo
+        {authToken && (
+          <ul>
+            <li>
+              <NavLink to="/">Back</NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">Profile & Preference</NavLink>
+            </li>
+            <li>
+              <NavLink to="/matches">Matches</NavLink>
+            </li>
+            <li>
+              <NavLink to="/chatpage">Chat</NavLink>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </>
   );
-}
+};
+
+export default NavBar;
