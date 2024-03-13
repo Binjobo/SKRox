a3fd6c5ee3b7dd0fc8481dcb5717243376b5f24f
Binjobo <101441601+Binjobo@users.noreply.github.com>
2024-03-13 11:23:57 +0800

    remove authenticate in admin router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/usersController");
 
-const { 
-  // isAdmin, 
-  authenticate } = require("../middleware/authMiddleware.js"); //new
+// const {
+//   isAdmin,
+//   authenticate,
+// } = require("../middleware/authMiddleware.js"); //new
 
 //get
 router.get("/user", userController.getUser);
@@ -25,7 +26,7 @@ router.delete("/removematch", userController.deleteMatch);
 // Admin routes
 router.get(
   "/admin/users",
-  authenticate,
+  // authenticate,
   // isAdmin,
   userController.getAllUsers
 );
